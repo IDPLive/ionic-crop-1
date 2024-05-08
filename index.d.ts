40d153c29093f4c1610cd59b122e6f38a442cdd6
Pudhiyavan0405 <102018919+Pudhiyavan0405@users.noreply.github.com>
2024-05-08 12:05:22 +0530

    ionic native core

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,8 @@
-import { AwesomeCordovaNativePlugin } from '@awesome-cordova-plugins/core';
-export interface CropOptions {
-    quality?: number;
-    targetHeight?: number;
-    targetWidth?: number;
-}
-/**
- * @name Crop
- * @description Crops images
- * @usage
- * ```typescript
- * import { Crop } from '@ionic-native/crop/ngx';
- *
- * constructor(private crop: Crop) { }
- *
- * ...
- *
- * this.crop.crop('path/to/image.jpg', {quality: 75})
- *   .then(
- *     newImage => console.log('new image path is: ' + newImage),
- *     error => console.error('Error cropping image', error)
- *   );
- * ```
- * @interfaces
- * CropOptions
- */
-export declare class CropOriginal extends AwesomeCordovaNativePlugin {
-    /**
-     * Crops an image
-     * @param {string} pathToImage
-     * @param {CropOptions} [options]
-     * @returns {Promise<string>} Returns a promise that resolves with the new image path, or rejects if failed to crop.
-     */
-    crop(pathToImage: string, options?: CropOptions): Promise<string>;
-}
-
-export declare const Crop: CropOriginal;
+export { IonicNativePlugin } from './ionic-native-plugin';
+export { checkAvailability, instanceAvailability, wrap, getPromise } from './decorators/common';
+export * from './decorators/cordova';
+export * from './decorators/cordova-function-override';
+export * from './decorators/cordova-instance';
+export * from './decorators/cordova-property';
+export * from './decorators/instance-property';
+export * from './decorators/interfaces';
